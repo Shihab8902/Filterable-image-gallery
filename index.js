ee719193c6fdb2966d6e7f8d082a27260252ffde
shihab8902 <shihabbd018@gmail.com>
2023-02-10 21:36:21 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll(".btn");
+const images = document.querySelectorAll(".image");
+buttons.forEach((button)=>{
+    button.addEventListener("click", (e)=>{
+        e.preventDefault();
+        let filter = e.target.dataset.filter;
+        images.forEach((item)=>{
+            if(filter === "all"){
+                item.style.display = "block";
+            }else{
+                if(item.classList.contains(filter)){
+                    item.style.display = "block";
+                }else{
+                    item.style.display = "none";
+                }
+            }
+        })
+    })
+})
